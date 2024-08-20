d7a25471e44967bf8180d0dee116d4f42778b077
c-inodeo <c.inodeo@arcanys.com>
2024-08-20 14:36:10 +0800

    20/08 - Commands Service changes

--- CommandsService/obj/Debug/net8.0/CommandsService.AssemblyInfo.cs
+++ CommandsService/obj/Debug/net8.0/CommandsService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommandsService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7d8089fb396b9cfa9c886b20908b97792f9adfc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cf43962abae25fc4e4f770397480175ba4eafa3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommandsService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommandsService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f357617ac0cc20bb304482862e3775f8c30cc6d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cf43962abae25fc4e4f770397480175ba4eafa3")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

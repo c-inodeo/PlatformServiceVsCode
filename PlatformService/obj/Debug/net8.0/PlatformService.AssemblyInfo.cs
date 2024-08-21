3ee056c946e79a76d379535424c1a5fe85dc71f6
c-inodeo <c.inodeo@arcanys.com>
2024-08-21 10:06:54 +0800

    Update 21/08 - Added docker file for commands service

--- PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7a25471e44967bf8180d0dee116d4f42778b077")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+126fb382ad1834bfebcd654f37883d610a60d1a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

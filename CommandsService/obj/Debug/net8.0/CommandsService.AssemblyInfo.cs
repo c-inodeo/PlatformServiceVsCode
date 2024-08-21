25f79162bf1e1cfa12be2a1a8605d5af5fd45c65
c-inodeo <c.inodeo@arcanys.com>
2024-08-21 10:13:31 +0800

    Update 21/08 - Update Dockerfile name

--- CommandsService/obj/Debug/net8.0/CommandsService.AssemblyInfo.cs
+++ CommandsService/obj/Debug/net8.0/CommandsService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommandsService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+126fb382ad1834bfebcd654f37883d610a60d1a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ee056c946e79a76d379535424c1a5fe85dc71f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommandsService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommandsService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

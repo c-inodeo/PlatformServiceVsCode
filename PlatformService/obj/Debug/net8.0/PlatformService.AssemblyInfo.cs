114043edffb8bb3c5d1ff19831e5a631ec4bf110
c-inodeo <c.inodeo@arcanys.com>
2024-08-29 13:42:28 +0800

    Update 29/08 - Added ingress-srv.yaml

--- PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+666aa93dcb07b488c763b1039c0f0969e1fba34a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fc7f4eb7527dfbd383f7b39968912eafcdae59d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

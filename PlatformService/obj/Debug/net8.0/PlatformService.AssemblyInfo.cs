b84b8ba904fb7ef435145df6ca50bc83532f9c7e
c-inodeo <c.inodeo@arcanys.com>
2024-09-02 15:45:01 +0800

    Update 02/09 - Update for Ingress fix

--- PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+114043edffb8bb3c5d1ff19831e5a631ec4bf110")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4b8ffdf91e83644313c7ae5273361a3aea2d5ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

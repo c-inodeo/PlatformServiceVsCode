480f311af79eb7a333867ad0fa1f662d176a3e1b
c-inodeo <c.inodeo@arcanys.com>
2024-09-03 17:22:45 +0800

    Update 03/09 - Added Dtos, Models, and updated controller for GetAllPlatforms endpoint

--- CommandsService/Program.cs
+++ CommandsService/Program.cs
@@ -1,6 +1,12 @@
+using CommandsService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+builder.Services.AddScoped<ICommandRepo, CommandRepo>();
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();

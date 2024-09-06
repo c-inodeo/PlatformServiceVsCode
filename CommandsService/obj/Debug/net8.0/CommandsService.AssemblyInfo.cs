8b38cf37ac8c3e2fefdbc6c14a057548081efd3f
c-inodeo <c.inodeo@arcanys.com>
2024-09-06 11:26:30 +0800

    Update 06/09 - Added grpc files for platformservice and commandservice csproj

--- CommandsService/obj/Debug/net8.0/CommandsService.AssemblyInfo.cs
+++ CommandsService/obj/Debug/net8.0/CommandsService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommandsService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+700fb3795a2519356ff8073ac7b8850bd9f383ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93e225dda574a3179579ecd494afc5372f01d80f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommandsService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommandsService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- PlatformService/Profiles/PlatformProfile.cs
+++ PlatformService/Profiles/PlatformProfile.cs
@@ -8,9 +8,14 @@ namespace PlatformService.Profiles
     {
         public PlatformProfile()
         {
+            // Syntax: <Source -> Target>
             CreateMap<Platform, PlatformReadDto>();
             CreateMap<PlatformCreateDto, Platform>();
             CreateMap<PlatformReadDto, PlatformPublishedDto>();
+            CreateMap<Platform, GrpcPlatformModel>() //mapped from Protos/platforms.proto
+                .ForMember(dest => dest.PlatformId, opt=> opt.MapFrom(src => src.Id))
+                .ForMember(dest => dest.Name, opt=> opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Publisher, opt=> opt.MapFrom(src => src.Publisher));
         }
     }
 }
